5078b03c7982098d63f9fe9701c8576cc5784909
Михаил Масный <masny.mikhail@gmail.com>
2019-05-16 21:34:02 +0300

    Added basic implementation classes types of List.

---
+++ DataStructures.BL/List/DuplexItemList.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructures.BL.List
+{
+    public class DuplexItemList<T>
+    {
+        public T Data { get; set; }
+        public DuplexItemList<T> Previous { get; set; }
+        public DuplexItemList<T> Next { get; set; }
+
+        public DuplexItemList(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ DataStructures.BL/List/ItemList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataStructures.BL.List
+{
+    public class ItemList<T>
+    {
+        private T data = default(T);
+
+        public T Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                if (value != null)
+                    data = value;
+                else
+                    throw new ArgumentNullException(nameof(value));
+            }
+        }
+
+        public ItemList<T> Next { get; set; }
+
+        public ItemList(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
