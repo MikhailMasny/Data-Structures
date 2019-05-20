5061c0b8eb5fb83441e6cdfed1022d6980b3658d
Михаил Масный <masny.mikhail@gmail.com>
2019-05-20 11:55:30 +0300

    Added basic implementation class BinarySearchTree. Added XML comments to main class BinarySearchTree.

---
+++ DataStructures.BL/BinarySearchTree/Node.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace DataStructures.BL.BinarySearchTree
+{
+    /// <summary>
+    /// Элемент BinarySearchTree.
+    /// </summary>
+    /// <typeparam name="T">Обобщение.</typeparam>
+    public class Node<T> : IComparable<T>, IComparable where T : IComparable, IComparable<T>
+    {
+        /// <summary>
+        /// Данные (дерево).
+        /// </summary>
+        public T Data { get; private set; }
+
+        /// <summary>
+        /// Левый узел.
+        /// </summary>
+        public Node<T> Left { get; private set; }
+
+        /// <summary>
+        /// Правый узел.
+        /// </summary>
+        public Node<T> Right { get; private set; }
+
+        /// <summary>
+        /// Конструктор c данными.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Конструктор с данными и навигацией.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        /// <param name="left">Влево.</param>
+        /// <param name="right">Вправо.</param>
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        /// <summary>
+        /// Добавить элемент.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Сравнить объект.
+        /// </summary>
+        /// <param name="obj">Объект.</param>
+        /// <returns></returns>
+        public int CompareTo(object obj)
+        {
+            if (obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new ArgumentException("No type match.");
+            }
+        }
+
+        /// <summary>
+        /// Сравнить объект.
+        /// </summary>
+        /// <param name="other">Другой элемент.</param>
+        /// <returns></returns>
+        public int CompareTo(T other)
+        {
+            return Data.CompareTo(other);
+        }
+
+        /// <summary>
+        /// Переопределение ToString.
+        /// </summary>
+        /// <returns>Определенного вида строка.</returns>
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ DataStructures.BL/BinarySearchTree/Tree.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.BL.BinarySearchTree
+{
+    /// <summary>
+    /// Дерево (BinarySearchTree).
+    /// </summary>
+    /// <typeparam name="T">Обобщение.</typeparam>
+    public class Tree<T> where T : IComparable, IComparable<T>
+    {
+        /// <summary>
+        /// Корень.
+        /// </summary>
+        public Node<T> Root { get; private set; }
+
+        /// <summary>
+        /// Количество элементов.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Добавить элемент.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        public void Add(T data)
+        {
+            if (Root == null)
+            {
+                Root = new Node<T>(data);
+                Count = 1;
+                return;
+            }
+
+            Root.Add(data);
+            Count++;
+        }
+
+        /// <summary>
+        /// Префиксный обход.
+        /// </summary>
+        /// <returns>Список.</returns>
+        public List<T> Preorder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return Preorder(Root);
+        }
+
+        /// <summary>
+        /// Постфиксный обход.
+        /// </summary>
+        /// <returns></returns>
+        public List<T> Postorder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return Postorder(Root);
+        }
+
+        /// <summary>
+        /// Инфиксный обход.
+        /// </summary>
+        /// <returns>Список.</returns>
+        public List<T> Inorder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+
+            return Inorder(Root);
+        }
+
+        private List<T> Preorder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                list.Add(node.Data);
+
+                if (node.Left != null)
+                {
+                    list.AddRange(Preorder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Preorder(node.Right));
+                }
+            }
+
+            return list;
+        }
+
+        private List<T> Postorder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(Postorder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Postorder(node.Right));
+                }
+
+                list.Add(node.Data);
+            }
+
+            return list;
+        }
+
+        private List<T> Inorder(Node<T> node)
+        {
+            var list = new List<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(Inorder(node.Left));
+                }
+
+                list.Add(node.Data);
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Inorder(node.Right));
+                }
+            }
+
+            return list;
+        }
+    }
+}
